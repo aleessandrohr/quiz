0433d92cfb25a9157c29ed541279404a0f2899da
Alessandro Ramos <toprime13@gmail.com>
2020-11-18 09:56:49 -0300

    adicionado parte visual

---
+++ código/scripts/script.js
@@ -0,0 +1,17 @@
+function corentrar(cor) {
+    if (cor == 1) {
+        var area = document.getElementById('a1')
+    } else {
+        var area = document.getElementById('a2')
+    }
+    area.style.background= 'rgba(48, 48, 233, 0.726)'
+}
+
+function corsair(cor){
+    if (cor == 1) {
+        var area = document.getElementById('a1')
+    } else {
+        var area = document.getElementById('a2')
+    }
+    area.style.background= 'rgba(30, 61, 199, 0.397)'
+}
